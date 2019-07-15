f1d470071ad41bfeabf025e2066cf2ec7f041d6c
juarmas <juarmas@juarmas.com>
2019-07-15 08:25:41 -0300

    no se por que no se subio esto!

---
+++ src/main/java/api/config/EntityConfiguration.java
@@ -0,0 +1,17 @@
+package api.config;
+
+import services.UserService;
+
+public enum EntityConfiguration {
+	
+	USER{
+		@Override
+		public Class<?> getEntityService() {
+			return UserService.class;
+		}
+
+	};
+	
+	
+	public abstract Class<?> getEntityService();
+}
